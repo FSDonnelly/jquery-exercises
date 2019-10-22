567f80845387782239803797670d9b6715251598
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-22 12:51:13 -0700

    add style using jquery exercises

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 // Style with JQuery
 let btn = $('.btn');
+let block = $('.block');
+let block2 = $('.block-2');
+
 btn.on('click', () => {
-  let block = $('.block');
   let style = {
     boxShadow: '0 0 40px rgba(0, 0, 0, 0.8)',
     borderRadius: '180px',
     height: '400px',
     width: '400px',
-    margin: '0 auto',
-    backgroundImage: 'url(js/batman.png)'
+    margin: '0 auto'
   };
-  block.css(style).toggle('slow', 'linear');
+  block.css(style);
+  block2.css({
+    width: block.css('width'),
+    height: block.css('height'),
+    backgroundColor: 'yellow'
+  });
 });
