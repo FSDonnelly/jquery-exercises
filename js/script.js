1505e94cd1e8f07d98a869eda6e470ba3842ebb3
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-21 22:05:25 -0700

    example of jquery style func

--- js/script.js
+++ js/script.js
@@ -1,34 +1,3 @@
-// Events
-
-// Mouse Events
-// click, dblclick, mouseover
-
-// Keyboard Events
-// keypress, keyup, keydown
-
-// Form Events
-// submit, change, focus
-
-// Document / Window Events
-// load, resize, scroll
-
-// Window Events
-
-$(window).on('load', () => {
-  console.log('The page has loaded');
-});
-
-$(window).on('resize', () => {
-  console.log(`Width: ${$(window).width()}; Height: ${$(window).height()}`);
-});
-
-$(window).on('scroll', () => {
-  console.log(`Offset top: ${$(window).scrollTop()}`);
-});
-
-$(document).ready(() => {
-  let h1 = $('h1');
-  console.log(h1);
-  let a = $('a.blue-link');
-  console.log(a);
-});
+// Style with JQuery
+let block = $('.block');
+block.css('box-shadow', '0 0 40px rgba(0, 0, 0, 0.8)');
