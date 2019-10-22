51a441b36d83b6be8fa9bddf852959f26a70a506
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-22 00:16:17 -0700

    example of jquery style func

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 // Style with JQuery
-let block = $('.block');
-block.css({
-  'box-shadow': '0 0 40px rgba(0, 0, 0, 0.8)',
-  'border-radius': '180px',
-  height: '400px',
-  width: '400px',
-  margin: '0 auto',
-  'background-image': 'url(js/batman.png)'
+let btn = $('.btn');
+btn.on('click', () => {
+  let block = $('.block');
+  let style = {
+    boxShadow: '0 0 40px rgba(0, 0, 0, 0.8)',
+    borderRadius: '180px',
+    height: '400px',
+    width: '400px',
+    margin: '0 auto',
+    backgroundImage: 'url(js/batman.png)'
+  };
+  block.css(style).toggle('slow', 'linear');
 });
