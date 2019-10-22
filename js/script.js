c3b7c710484de7f94bac67404449d6656592f0a6
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-22 13:27:01 -0700

    add visual effects using jquery exercises

--- js/script.js
+++ js/script.js
@@ -1,15 +1,52 @@
 // Visual Effects
 let hideBtn = $('.hideBlock');
 let showBtn = $('.showBlock');
+let toggle = $('.toggle');
 let block = $('#block');
 let block2 = $('#block-2');
 
-hideBtn.on('click', () => {
-  block.slideUp();
-  block2.slideDown();
-});
+//Other Func
+// -- fadeIn(), fadeOut(), hide(), show(), slideUp(), slideDown()
+// -- toggle() = {show(), hide()}
+// -- fadeToggle() = {fadeIn(), fadeOut()}
+// -- slideToggle() = {slideDown(), slideUp()}
 
-showBtn.on('click', () => {
-  block.slideDown();
-  block2.slideUp();
+// hideBtn.on('click', () => {
+//   block.slideUp();
+//   block2.slideDown();
+// });
+
+// showBtn.on('click', () => {
+//   block.slideDown();
+//   block2.slideUp();
+// });
+
+// hideBtn.on('click', () => {
+//   block.fadeIn();
+//   block2.fadeOut();
+// });
+
+// showBtn.on('click', () => {
+//   block.fadeOut();
+//   block2.fadeIn();
+// });
+
+// hideBtn.on('click', () => {
+//   block.hide();
+//   block2.show();
+// });
+
+// showBtn.on('click', () => {
+//   block.show();
+//   block2.hide();
+// });
+
+// toggle.on('click', () => {
+//   block.toggle();
+//   block2.toggle();
+// });
+
+toggle.on('click', () => {
+  block.fadeToggle();
+  block2.slideToggle();
 });
