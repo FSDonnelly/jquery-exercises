4d692ce2b792fead6ecaae7a4e4d915691d97ca4
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-21 17:37:29 -0700

    example of jquery mouse event funcs

--- js/script.js
+++ js/script.js
@@ -4,3 +4,27 @@ $(document).ready(() => {
   let a = $('a.blue-link');
   console.log(a);
 });
+
+// Events
+// Mouse Events
+// click, dblclick, mouseover
+// Keyboard Events
+// keypress, keyup, keydown
+// Form Events
+// submit, change, focus
+// Document / Window Events
+// load, resize, scroll
+
+// Mouse Events
+
+// <button class='clickBtn'>Click</button>;
+let button1 = $('.clickBtn');
+let button2 = $('.dblclickBtn');
+
+button1.click(() => {
+  alert('You have single-clicked!');
+});
+
+button2.on('dblclick', () => {
+  alert('You have double-clicked!');
+});
