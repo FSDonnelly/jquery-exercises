34073652658399e95961363fd3ff53045b2189ab
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-23 18:26:25 -0700

    toggleClass() jquery exercises

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-// addClass() removeClass()
+// addClass() removeClass() toggleClass()
 let btn = $('.btn1');
 let btn2 = $('.btn2');
+let btn3 = $('.btn3');
 
 let block = $('.block');
 
 btn.on('click', () => {
-  block.addClass('red-text');
+  block.addClass('red-back');
 });
 
 btn2.on('click', () => {
-  block.removeClass('red-text');
+  block.removeClass('red-back');
+});
+
+btn3.on('click', () => {
+  block.toggleClass('red-back');
 });
